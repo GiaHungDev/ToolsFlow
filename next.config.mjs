f3fc611850a4eb9d84122856b0c3945299b69a5e
hungpd <hungpd@vfastsoft.com>
2026-05-19 16:52:08 +0700

    Fix standalone output for pipeline build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // output: "standalone",
+  output: "standalone",
   eslint: {
     // Allows production builds to complete even if ESLint errors are present
     ignoreDuringBuilds: true,
